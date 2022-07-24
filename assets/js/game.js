514a132a1061f5bb9aa821cccab9df6f99f523c7
paul lovato <lovato.paul97@yahoo.com>
2022-07-23 23:44:11 -0400

    began working on values and console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("this is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name?")
+
+console.log(playerName)
+
+console.log("This Logs a string, good for leaving yourself a message")
+// this will do math and log in a 20
+console.log(10 + 10)
+// What is this?
+console.log("our robot's name is + playerName")
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
