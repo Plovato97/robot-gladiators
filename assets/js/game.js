e780e3a68c5385747393014a5a469833e086a7b3
paul lovato <lovato.paul97@yahoo.com>
2022-07-28 18:03:53 -0400

    Shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,14 +35,19 @@ var startGame = function() {
        }
     }
     
+fight(pickedEnemyName);
+
+    // if we're not at the last enemy in the array
+    if (i < enemyNames.length - 1) {
+        shop()
+    }
     // play again
-    endGame();
 };
 
-    // fuction to end the entire game
+    // function to end the entire game
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!")
-    // if player is stil lalive, Player wins!
+    // if player is stil alive, Player wins!
     if (playerHealth > 0) {
         window.alert("Great Job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
@@ -60,6 +65,48 @@ if (playAgainConfirm) {
 else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
+};
+
+var shop = function() {
+  console.log("entered the shop");
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch(shopOptionPrompt) {
+      case "refill":
+      case "REFILL": 
+      if (playerMoney >= 7) {
+        window.alert("Refillin player's Health by 20 for 7 dollars.")
+          // increase health and decrease money
+          playerHealth = playerHealth + 20;
+          playerMoney = playerMoney - 7;
+      }else {
+        window.alert("You don't have enough money!")
+      }
+        break;
+      case "upgrade":
+      case "UPGRADE":
+        if (playerMoney >= 7){
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        // increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+        }else {
+          window.alert("You don't have enough money!")
+        }
+        break;
+      case "leave":
+      case "LEAVE":
+        window.alert("Leaving the store.");
+        // Do nothing, sop function will end
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        // call shop() again to force player to pick a valid option
+        shop();
+        break;
+  }
 };
 
 // fight function (now with parameter for enemy's name)
@@ -82,13 +129,13 @@ var fight = function(enemyName) {
         break;
       }
     }
-
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-    );
-
+ // remove enemy's health by subtracting the amount set in the playerAttack variable
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+      enemyHealth = enemyHealth - playerAttack;
+      console.log(
+        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+      );
+    }
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(enemyName + ' has died!');
@@ -103,11 +150,12 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
+    if (promptFight === "fight" || promptFight === "FIGHT") {
     playerHealth = playerHealth - enemyAttack;
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
-
+  }
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + ' has died!');
@@ -116,7 +164,7 @@ var fight = function(enemyName) {
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
-  } // end of while loop
+  } 
 }; // end of fight function
 
 // fight each enemy-robot by looping over them and fighting them one at a time
